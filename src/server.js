b25ceaa96fe7b5f85f634c4187c648759804db25
Yeongil Yu <polosesy@nate.com>
2020-06-10 18:04:07 +0900

    2.0 Hello World part One

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
 app.use(express.static(join(__dirname, "static")));
+app.use(logger("dev"));
 app.get("/", (req, res) => res.render("home"));
 
 const handleListening = () =>
@@ -14,4 +16,10 @@ const handleListening = () =>
 
 const server = app.listen(PORT, handleListening);
 
-const io = socketIO(server);
+const io = socketIO.listen(server);
+
+let sockets = [];
+
+io.on("connection", (socket) => {
+  sockets.push(socket.id);
+});
